ddf6ab16c1500480c3f309edd8bcb5d6be23589d
Lhk <212792772@qq.com>
2024-02-24 23:00:47 +0800

    Feat: 新增 全局通用布局

---
+++ src/components/BasicHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="basic-header">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="title-logo" src="../assets/logo.png" />
+          <div class="title">kkoj</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title-logo {
+  height: 50px;
+  cursor: pointer;
+}
+
+.title {
+  margin-left: 2px;
+  color: #42b983;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-layout style="height: 400px">
+    <a-layout-header>
+      <BasicHeader />
+    </a-layout-header>
+    <a-layout-content>
+      <router-view />
+    </a-layout-content>
+    <a-layout-footer>
+      <a href="https://lhk1008611.github.io/" target="_blank">
+        作者: HankLay
+      </a>
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<style></style>
+<script>
+import BasicHeader from "@/components/BasicHeader";
+
+export default {
+  components: { BasicHeader },
+};
+</script>
